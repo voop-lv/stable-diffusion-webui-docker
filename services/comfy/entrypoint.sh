995be5419c236e74cba72d2259f5223867bfe28a
Voop <viperlv2@inbox.lv>
2023-07-18 21:19:19 +0300

    Adding better custom node requirements processor

--- services/comfy/entrypoint.sh
+++ services/comfy/entrypoint.sh
@@ -3,12 +3,38 @@
 set -Eeuo pipefail
 
 mkdir -vp /data/config/comfy/custom_nodes
+mkdir -vp /stable-diffusion/custom_nodes
 
 declare -A MOUNTS
 
 MOUNTS["/root/.cache"]="/data/.cache"
 MOUNTS["${ROOT}/input"]="/data/config/comfy/input"
+MOUNTS["${ROOT}/custom_nodes"]="/data/config/comfy/custom_nodes"
 MOUNTS["${ROOT}/output"]="/output/comfy"
+
+install_requirements() {
+    local dir="$1"
+    if [[ -f "${dir}/requirements.txt" ]]; then
+        echo "Installing requirements for ${dir}..."
+        pip install -r "${dir}/requirements.txt"
+        echo "Requirements installed for ${dir}."
+    else
+        echo "No requirements.txt found in ${dir}. Skipping."
+    fi
+}
+
+process_directory() {
+    local dir="$1"
+    if [[ -d "${dir}" ]]; then
+        for sub_dir in "${dir}"/*; do
+            if [[ -d "${sub_dir}" ]]; then
+                install_requirements "${sub_dir}"
+            fi
+        done
+    else
+        echo "Error: ${dir} is not a directory."
+    fi
+}
 
 for to_path in "${!MOUNTS[@]}"; do
   set -Eeuo pipefail
@@ -21,5 +47,7 @@ for to_path in "${!MOUNTS[@]}"; do
   ln -sT "${from_path}" "${to_path}"
   echo Mounted $(basename "${from_path}")
 done
+
+process_directory "${ROOT}/custom_nodes"
 
 exec "$@"
