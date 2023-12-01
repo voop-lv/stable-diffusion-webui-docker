e031c04bc1fff0dfdf7496e5f8f6e680f53a3c63
Voop <viperlv2@inbox.lv>
2023-12-01 15:00:49 +0200

    Updated Mounts

--- services/fooocus/mre/mount.sh
+++ services/fooocus/mre/mount.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
 declare -A MOUNTS
+ROOT_PAHT="/fooocus-ai"
 
+mkdir /data/models/model_configs
 
+MOUNTS["${ROOT}/outputs"]="/output"
+
+MOUNTS["${ROOT}/models/checkpoints"]="/data/models/Stable-diffusion"
+MOUNTS["${ROOT}/models/controlnet"]="/data/models/ControlNet"
+MOUNTS["${ROOT}/models/loras"]="/data/models/Lora"
+MOUNTS["${ROOT}/models/gligen"]="/data/models/GLIGEN"
+MOUNTS["${ROOT}/models/embeddings"]="/data/models/embeddings"
+MOUNTS["${ROOT}/models/vae"]="/data/models/VAE"
+MOUNTS["${ROOT}/models/vae_approx"]="/data/models/VAE-approx"
+MOUNTS["${ROOT}/models/hypernetworks"]="/data/models/hypernetworks"
+MOUNTS["${ROOT}/models/upscale_models"]="/data/models/upscale_models"
+MOUNTS["${ROOT}/models/configs"]="/data/models/model_configs"
 
 for to_path in "${!MOUNTS[@]}"; do
   set -Eeuo pipefail
