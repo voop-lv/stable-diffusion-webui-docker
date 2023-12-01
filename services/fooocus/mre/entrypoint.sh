5408378afe7ed57f3ff4b3bc98d7ccdd23004b04
Voop <viperlv2@inbox.lv>
2023-12-01 15:06:48 +0200

    Updated Entrypoint

--- services/fooocus/mre/entrypoint.sh
+++ services/fooocus/mre/entrypoint.sh
@@ -3,6 +3,7 @@
 set -Eeuo pipefail
 
 SETTINGS_FILEPATH="/data/config/fooocus-mre/settings.json"
+SDXL_STYLES="/data/config/fooocus-mre/sdxl_styles"
 ROOT_PAHT="/fooocus-ai"
 
 checkDir() {
@@ -25,6 +26,18 @@ if ! checkFile $SETTINGS_FILEPATH; then
         echo "[BOOTSTRAP] Failed to copy. Exiting"
         exit 1 
     fi
+fi
+
+echo "[BOOTSTRAP] Validating sdxl_styles"
+if ! checkDir $SDXL_STYLES; then 
+    echo "[BOOTSTRAP] Failed to find sdxl_styles in the config path. Copying a fresh copy."
+    cp -r -f -v /cleanConfig/sdxl_styles $SDXL_STYLES
+    if checkDir $SDXL_STYLES; then
+        echo "[BOOTSTRAP] Clean folder was copied"
+    else
+        echo "[BOOTSTRAP] Failed to copy. Exiting"
+        exit 1 
+    fi 
 fi
 
 echo "[BOOTSTRAP] Copying settings.json"
